10cc074508e46f114cf863106de9f9119df14319
Ckopei <cameron@kopels.com>
2019-10-30 14:53:50 -0400

    Updates

---
+++ precache-manifest.886ab9b59620c9995b7eca28fc71971a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d064a0bffaa4db1c4587b12d73477d27",
+    "url": "/React-Game/index.html"
+  },
+  {
+    "revision": "8fd633f81c85f185badc",
+    "url": "/React-Game/static/css/main.219fa834.chunk.css"
+  },
+  {
+    "revision": "861be102a173b70e8a2b",
+    "url": "/React-Game/static/js/2.20bff340.chunk.js"
+  },
+  {
+    "revision": "8fd633f81c85f185badc",
+    "url": "/React-Game/static/js/main.317ca20d.chunk.js"
+  },
+  {
+    "revision": "2c53e72c1aae5220ac8f",
+    "url": "/React-Game/static/js/runtime-main.ec37026d.js"
+  }
+]);
